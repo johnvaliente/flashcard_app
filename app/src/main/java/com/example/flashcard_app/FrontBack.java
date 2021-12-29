cc3c7baf8332574fbbd0f8b1a46e2235353c9e68
johnvaliente <jvali735@mtroyal.ca>
2021-12-29 16:42:20 -0700

    updated xml files for showing cards

---
+++ app/src/main/java/com/example/flashcard_app/FrontBack.java
@@ -0,0 +1,10 @@
+package com.example.flashcard_app;
+
+/**
+ * this class will show the front and back of the card, need to use dynamic programming so
+ * when a user clicks a button it will show the back of the card, or go back to the previous card
+ *
+ *
+ */
+public class FrontBack {
+}
